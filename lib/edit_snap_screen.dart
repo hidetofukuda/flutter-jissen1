d11b795fe918903ff9fbfee5001c04ec31e62b2d
systemiFukuda <hideto.fukuda@systemi.co.jp>
2026-09-21 05:45:54 +0900

    画像の編集をできるようにした。

--- lib/edit_snap_screen.dart
+++ lib/edit_snap_screen.dart
@@ -2,15 +2,48 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'l10n/app_localizations.dart';
 
-class ImageEditScreen extends StatefulWidget {
-  const ImageEditScreen({super.key, required this.imageBitmap});
+import 'package:image/image.dart' as image_lib;
 
-  final Uint8List imageBitmap;
+class ImageEditScreen extends StatefulWidget {
+  const ImageEditScreen({super.key, required this._imageBitmap});
+
+  final Uint8List _imageBitmap;
+
   @override
   State<ImageEditScreen> createState() => _ImageEditScreenState();
+  
 }
-
 class _ImageEditScreenState extends State<ImageEditScreen> {
+
+ late Uint8List _imageBitmap;
+
+ @override
+ void initState(){
+  super.initState();
+  _imageBitmap = widget._imageBitmap;
+ }
+
+ void _rotateImage(){
+  // 
+  final image = image_lib.decodeImage(_imageBitmap);
+  if(image == null) return;
+  final rotatedImage = image_lib.copyRotate(image, angle:90);
+
+  setState(() {
+    _imageBitmap = Uint8List.fromList(image_lib.encodePng(rotatedImage));
+  });
+ }
+
+ void _flipImage(){
+  final image = image_lib.decodeImage(_imageBitmap);
+  if(image == null) return;
+  final flippedImage = image_lib.flipHorizontal(image);
+
+  setState(() {
+    _imageBitmap = Uint8List.fromList(image_lib.encodePng(flippedImage));
+  });
+ 
+ }
 
   @override
   Widget build(BuildContext context) {
@@ -24,18 +57,14 @@ class _ImageEditScreenState extends State<ImageEditScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.memory(widget.imageBitmap),
+            Image.memory(_imageBitmap, gaplessPlayback: true),
 
             IconButton(
-              onPressed: () {  // 画像を編集するボタン
-                // 例: 画像を左に回転させる
-              },
+              onPressed: () => _rotateImage(),
               icon: const Icon(Icons.rotate_left),
             ),
             IconButton(
-              onPressed: () {  // 画像を編集するボタン
-                //
-            },
+              onPressed: () => _flipImage(),
               icon: const Icon(Icons.flip),
             ),
           ],
